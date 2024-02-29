6565aa1db42cf32a0a42a3feb50442b0ea27f785
Nikita Alpatov <nikitaa95@mail.ru>
2024-02-29 12:08:31 +0200

    chor: add mock payments for test

--- src/features/profile-setting/ui/payments/payments.test.tsx
+++ src/features/profile-setting/ui/payments/payments.test.tsx
@@ -5,7 +5,7 @@ import { render, screen, waitFor } from '@testing-library/react'
 import { Payments } from './payments'
 
 import { useGetSubscriptionsQuery } from '@/shared/api/services/subscriptions/subscriptions.api'
-import { payments } from '@/shared/ui/table/constants'
+import { SubscriptionDataType } from '@/shared/api/services/subscriptions/subscriptions.api.types'
 
 jest.mock('@/shared/api/services/subscriptions/subscriptions.api', () => ({
   ...jest.requireActual('@/shared/api/services/subscriptions/subscriptions.api'),
@@ -27,3 +27,33 @@ describe('Payments', () => {
     })
   })
 })
+
+const payments = [
+  {
+    dateOfPayment: '10.10.2020',
+    endDateOfSubscription: '2.10.2020',
+    price: 20,
+    subscriptionId: '123',
+    subscriptionType: 'MONTHLY',
+    paymentType: 'STRIPE',
+    userId: 270,
+  },
+  {
+    dateOfPayment: '7.10.2020',
+    endDateOfSubscription: '9.10.2020',
+    price: 70,
+    subscriptionId: '123',
+    subscriptionType: 'DAY',
+    paymentType: 'STRIPE',
+    userId: 270,
+  },
+  {
+    dateOfPayment: '1.10.2020',
+    endDateOfSubscription: '6.10.2020',
+    price: 10,
+    subscriptionId: '123',
+    subscriptionType: 'WEEKLY',
+    paymentType: 'PAYPAL',
+    userId: 270,
+  },
+] as Array<SubscriptionDataType>
