e8df8a921426da1385dbb65e6dc4ce7622b7f6a6
Nikita Alpatov <nikitaa95@mail.ru>
2024-03-30 18:39:25 +0200

    fix: add mobile responsivness to forgot password page

--- src/features/auth-register/ui/forgot-pass-form/forgot-pass-form.tsx
+++ src/features/auth-register/ui/forgot-pass-form/forgot-pass-form.tsx
@@ -93,30 +93,37 @@ export function ForgotPass() {
               <p>{t('LinkHasBeenSent')}</p>
             </Modal>
           )}
-          {!isSentPass ? (
-            <Button size={ButtonSize.STRETCHED} className={styles.sendLinkBtn}>
-              {t('SendLink')}
-            </Button>
-          ) : (
-            <Button size={ButtonSize.STRETCHED} className={styles.sendLinkBtn}>
-              {t('SendAgain')}
-            </Button>
-          )}
-          <Link href={RoutersPath.signIn}>
-            <Button
-              className={styles.oppositeBtn}
-              theme={ButtonTheme.CLEAR}
-              size={ButtonSize.MIDDLE}
-            >
-              {t('BackToSignIn')}
-            </Button>
-          </Link>
-          <ReCAPTCHA
-            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
-            onChange={onChangeRecaptchaHandler}
-            theme={'dark'}
-            aria-required
-          />
+          <div className={styles.formContainer__btnsAndRecaptcha}>
+            <div className={styles.btns}>
+              {!isSentPass ? (
+                <Button size={ButtonSize.STRETCHED} className={styles.sendLinkBtn}>
+                  {t('SendLink')}
+                </Button>
+              ) : (
+                <Button size={ButtonSize.STRETCHED} className={styles.sendLinkBtn}>
+                  {t('SendAgain')}
+                </Button>
+              )}
+              <Link href={RoutersPath.signIn}>
+                <Button
+                  className={styles.oppositeBtn}
+                  theme={ButtonTheme.CLEAR}
+                  size={ButtonSize.MIDDLE}
+                >
+                  {t('BackToSignIn')}
+                </Button>
+              </Link>
+            </div>
+            <ReCAPTCHA
+              sitekey={
+                (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string) ||
+                '6LeY2y0mAAAAANwI_paCWfoksCgBm1n2z9J0nwNQ'
+              }
+              onChange={onChangeRecaptchaHandler}
+              theme={'dark'}
+              aria-required
+            />
+          </div>
         </form>
       </FormContainer>
     </>
