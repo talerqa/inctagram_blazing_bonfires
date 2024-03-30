a62e47cdaceb9466f941702a48df6b15148eed0d
Nikita Alpatov <nikitaa95@mail.ru>
2024-03-30 17:15:15 +0200

    fix: add mobile responsivness to merge account page

--- src/pages/merge-accounts/index.tsx
+++ src/pages/merge-accounts/index.tsx
@@ -8,7 +8,7 @@ import styles from './merge-accounts.module.scss'
 
 import broImage from '@/shared/assets/icons/login/bro.svg'
 import { getLayout } from '@/shared/layouts/main-layout/main-layout'
-import { Button, ButtonTheme } from '@/shared/ui'
+import { Button, ButtonSize, ButtonTheme, FormContainer } from '@/shared/ui'
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   if (locale === undefined) throw new Error()
@@ -22,15 +22,22 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
 const MergeAccountsPage = () => {
   return (
-    <div className={styles.mergeAccContainer}>
-      <h3>Merge of Accounts</h3>
-      <p>
-        The user with email <b>Epam@epam.com</b> is already in the system. Could we merge this
-        accounts?
-      </p>
-      <Button theme={ButtonTheme.CLEAR}>Yes, merge</Button>
-      <Button theme={ButtonTheme.CLEAR}>No</Button>
-      <Image src={broImage} alt={'women login accaunt in her phone'} />
+    <div className={styles.mergeAccounts}>
+      <FormContainer title={'Merge of Accounts'} className={styles.mergeAccountsContainer}>
+        <p>
+          The user with email <b>Epam@epam.com</b> is already in the system. Could we merge this
+          accounts?
+        </p>
+        <Image src={broImage} alt={'women login accaunt in her phone'} />
+        <div className={styles.mergeAccounts__btns}>
+          <Button theme={ButtonTheme.CLEAR} size={ButtonSize.STRETCHED}>
+            Yes, merge
+          </Button>
+          <Button theme={ButtonTheme.CLEAR} size={ButtonSize.STRETCHED}>
+            No
+          </Button>
+        </div>
+      </FormContainer>
     </div>
   )
 }
