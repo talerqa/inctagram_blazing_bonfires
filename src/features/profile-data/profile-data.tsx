5cf9a88d8ad2d026660a62b1d897c628eaa530ef
Nikita Alpatov <nikitaa95@mail.ru>
2024-04-05 17:30:08 +0300

    feat: make profile page mobile responsive

--- src/features/profile-data/profile-data.tsx
+++ src/features/profile-data/profile-data.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import Image from 'next/image'
 import router from 'next/router'
 import { useTranslation } from 'next-i18next'
+import { useSelector } from 'react-redux'
 
 import style from './profile-data.module.scss'
 
 import { ProfileFollowing } from '@/entities/profile-following'
+import { selectIsMobile } from '@/shared/api'
 import { ProfileUserType } from '@/shared/api/services/profile/profile.api.types'
 import noImage from '@/shared/assets/icons/avatar-profile/not-photo.png'
 import { ShortLangs } from '@/shared/types/lang-switcher-types'
@@ -19,33 +21,56 @@ export const ProfileData = ({ profileData }: Props) => {
   const {
     i18n: { t: tRoot, language },
   } = useTranslation('common', { keyPrefix: 'Profile' })
+  const isMobile = useSelector(selectIsMobile)
 
   return (
-    <div className={style.headerContainer}>
-      <div className={style.avatarContainer}>
-        <Image
-          src={profileData?.avatars?.[0]?.url ?? noImage}
-          alt={'avatar'}
-          width={204}
-          height={204}
-        />
-      </div>
-      <div className={style.profileInfoContainer}>
-        <div className={style.profileTitleContainer}>
-          <div>{profileData?.userName}</div>
-          <Button
-            className={style.buttonProfileSetting}
-            style={language === ShortLangs.RU ? { fontSize: '0.875rem' } : undefined}
-            onClick={() => router.push(`profile/general-information`)}
-          >
-            {tRoot('ProfileSetting')}
-          </Button>
+    <div className={style.headerWrapper}>
+      <div className={style.headerContainer}>
+        <div className={style.avatarContainer}>
+          <Image
+            src={profileData?.avatars?.[0]?.url ?? noImage}
+            alt={'avatar'}
+            width={204}
+            height={204}
+          />
         </div>
-        <ProfileFollowing amountFollowing={2128} amountFollowers={2128} amountPublications={2128} />
-        <div className={style.textWrapper}>
-          <Text size={'regular'}>{profileData?.aboutMe}</Text>
+        <div className={style.profileInfoContainer}>
+          <div className={style.profileTitleContainer}>
+            {!isMobile && (
+              <>
+                <b>{profileData?.userName}</b>
+                <Button
+                  className={style.buttonProfileSetting}
+                  style={language === ShortLangs.RU ? { fontSize: '0.875rem' } : undefined}
+                  onClick={() => router.push(`profile/general-information`)}
+                >
+                  {tRoot('ProfileSetting')}
+                </Button>
+              </>
+            )}
+          </div>
+          <ProfileFollowing
+            amountFollowing={2128}
+            amountFollowers={2128}
+            amountPublications={2128}
+          />
+          {!isMobile && (
+            <div className={style.textWrapperMobile}>
+              <Text size={'regular'}>{profileData?.aboutMe}</Text>
+            </div>
+          )}
         </div>
       </div>
+      {isMobile && (
+        <>
+          <div className={style.username}>
+            <b>{profileData?.userName}</b>
+          </div>
+          <div className={style.textWrapperMobile}>
+            <Text size={'regular'}>{profileData?.aboutMe}</Text>
+          </div>
+        </>
+      )}
     </div>
   )
 }
