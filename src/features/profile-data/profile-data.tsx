66ff9e24381a035e6f1439ca4c5a71847ba3eb7d
Rudyk Alexander <dezo103@gmail.com>
2024-04-15 11:54:27 +0300

    BB-174 ref: insert loader

--- src/features/profile-data/profile-data.tsx
+++ src/features/profile-data/profile-data.tsx
@@ -11,7 +11,7 @@ import { ProfileUserType } from '@/shared/api/services/profile/profile.api.types
 import { useGetUserDataQuery } from '@/shared/api/services/search/search.api'
 import noImage from '@/shared/assets/icons/avatar-profile/not-photo.png'
 import { ShortLangs } from '@/shared/types/lang-switcher-types'
-import { Button, Text } from '@/shared/ui'
+import { Button, LinearLoader, Text } from '@/shared/ui'
 
 type Props = {
   profileData: ProfileUserType | undefined
@@ -21,38 +21,41 @@ export const ProfileData = ({ profileData }: Props) => {
     i18n: { t: tRoot, language },
   } = useTranslation('common', { keyPrefix: 'Profile' })
 
-  const { data: userData } = useGetUserDataQuery({ userName: profileData?.userName })
+  const { data: userData, isLoading } = useGetUserDataQuery({ userName: profileData?.userName })
 
   return (
-    <div className={style.headerContainer}>
-      <div className={style.avatarContainer}>
-        <Image
-          src={profileData?.avatars?.[0]?.url ?? noImage}
-          alt={'avatar'}
-          width={204}
-          height={204}
-        />
-      </div>
-      <div className={style.profileInfoContainer}>
-        <div className={style.profileTitleContainer}>
-          <div>{profileData?.userName}</div>
-          <Button
-            className={style.buttonProfileSetting}
-            style={language === ShortLangs.RU ? { fontSize: '0.875rem' } : undefined}
-            onClick={() => router.push(`profile/general-information`)}
-          >
-            {tRoot('ProfileSetting')}
-          </Button>
+    <>
+      {isLoading && <LinearLoader />}
+      <div className={style.headerContainer}>
+        <div className={style.avatarContainer}>
+          <Image
+            src={profileData?.avatars?.[0]?.url ?? noImage}
+            alt={'avatar'}
+            width={204}
+            height={204}
+          />
         </div>
-        <ProfileFollowing
-          amountFollowing={userData?.followingCount}
-          amountFollowers={userData?.followersCount}
-          amountPublications={userData?.publicationsCount}
-        />
-        <div className={style.textWrapper}>
-          <Text size={'regular'}>{profileData?.aboutMe}</Text>
+        <div className={style.profileInfoContainer}>
+          <div className={style.profileTitleContainer}>
+            <div>{profileData?.userName}</div>
+            <Button
+              className={style.buttonProfileSetting}
+              style={language === ShortLangs.RU ? { fontSize: '0.875rem' } : undefined}
+              onClick={() => router.push(`profile/general-information`)}
+            >
+              {tRoot('ProfileSetting')}
+            </Button>
+          </div>
+          <ProfileFollowing
+            amountFollowing={userData?.followingCount}
+            amountFollowers={userData?.followersCount}
+            amountPublications={userData?.publicationsCount}
+          />
+          <div className={style.textWrapper}>
+            <Text size={'regular'}>{profileData?.aboutMe}</Text>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/features/public-profile/ui/public-profile-data/public-profile-data.tsx
+++ src/features/public-profile/ui/public-profile-data/public-profile-data.tsx
@@ -11,7 +11,7 @@ import { PublicProfileType } from '@/shared/api/services/public/public.api.types
 import { useFollowUserMutation, useGetUserDataQuery } from '@/shared/api/services/search/search.api'
 import noImage from '@/shared/assets/icons/image/no-image.svg'
 import { useTruncateText } from '@/shared/hooks'
-import { Button, ButtonTheme, Text } from '@/shared/ui'
+import { Button, ButtonTheme, LinearLoader, Text } from '@/shared/ui'
 
 type PropsType = {
   data: PublicProfileType
@@ -32,64 +32,69 @@ export const PublicProfileData = (props: PropsType) => {
   const notImageClass = clsx(style.avatar, !avatars[0] && style.notAvatar)
 
   const { data: userData } = useGetUserDataQuery({ userName })
-  const [followUser, { isLoading: followUserLoading, error: errorFollowUser }] =
-    useFollowUserMutation()
+  const [followUser, { isLoading, error: errorFollowUser }] = useFollowUserMutation()
   const { t } = useTranslation('common', { keyPrefix: 'SearchPage' })
 
   return (
-    <div className={style.profileContainer}>
-      <div className={style.avatarContainer}>
-        <Image
-          src={avatars[0]?.url ?? noImage}
-          alt={'avatar'}
-          width={204}
-          height={204}
-          className={notImageClass}
-          priority
-        />
-      </div>
-      <div className={style.profileDataContainer}>
-        <div className={style.profileDataHeader}>
-          <Text as={'p'} size={'xxl'} weight={'bold'} className={style.userName}>
-            {userName}
-          </Text>
-          <div className={style.buttonsContainer}>
-            {!userData?.isFollowing ? (
-              <Button onClick={() => followUser({ selectedUserId: id })}>{t('Follow')}</Button>
-            ) : (
-              <Button theme={ButtonTheme.CLEAR} onClick={() => followUser({ selectedUserId: id })}>
-                {t('Unfollow')}
-              </Button>
-            )}
-
-            <Button>SendMessage</Button>
-          </div>
-        </div>
-        <div className={style.profileInfo}>
-          <ProfileFollowing
-            amountFollowing={userData?.followingCount}
-            amountFollowers={userData?.followersCount}
-            amountPublications={amountPost}
+    <>
+      {isLoading && <LinearLoader />}
+      <div className={style.profileContainer}>
+        <div className={style.avatarContainer}>
+          <Image
+            src={avatars[0]?.url ?? noImage}
+            alt={'avatar'}
+            width={204}
+            height={204}
+            className={notImageClass}
+            priority
           />
         </div>
+        <div className={style.profileDataContainer}>
+          <div className={style.profileDataHeader}>
+            <Text as={'p'} size={'xxl'} weight={'bold'} className={style.userName}>
+              {userName}
+            </Text>
+            <div className={style.buttonsContainer}>
+              {!userData?.isFollowing ? (
+                <Button onClick={() => followUser({ selectedUserId: id })}>{t('Follow')}</Button>
+              ) : (
+                <Button
+                  theme={ButtonTheme.CLEAR}
+                  onClick={() => followUser({ selectedUserId: id })}
+                >
+                  {t('Unfollow')}
+                </Button>
+              )}
 
-        <div className={style.profileTitleContainer}>
-          <Text as={'p'} size={'regular'} weight={'regular'} className={style.fullText}>
-            {aboutMe ? fullText : ''}{' '}
-            {displayShowMore && (
-              <Text
-                onClick={() => setIsShowMoreActive(!isShowMoreActive)}
-                size={'link'}
-                color={'info'}
-                as={'span'}
-                className={style.showMore}
-              >
-                {isShowMoreActive ? 'Hide' : 'Show more'}
-              </Text>
-            )}
-          </Text>
+              <Button>SendMessage</Button>
+            </div>
+          </div>
+          <div className={style.profileInfo}>
+            <ProfileFollowing
+              amountFollowing={userData?.followingCount}
+              amountFollowers={userData?.followersCount}
+              amountPublications={amountPost}
+            />
+          </div>
+
+          <div className={style.profileTitleContainer}>
+            <Text as={'p'} size={'regular'} weight={'regular'} className={style.fullText}>
+              {aboutMe ? fullText : ''}{' '}
+              {displayShowMore && (
+                <Text
+                  onClick={() => setIsShowMoreActive(!isShowMoreActive)}
+                  size={'link'}
+                  color={'info'}
+                  as={'span'}
+                  className={style.showMore}
+                >
+                  {isShowMoreActive ? 'Hide' : 'Show more'}
+                </Text>
+              )}
+            </Text>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
