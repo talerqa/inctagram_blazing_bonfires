bf1b58ad3676b45ae15d834e8be6a136752d6258
Rudyk Alexander <dezo103@gmail.com>
2024-05-08 23:33:04 +0300

    BB-218 fix: implement correct redirect

--- src/pages/super-admin/index.tsx
+++ src/pages/super-admin/index.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useLayoutEffect } from 'react'
+
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import { RoutersPath } from '@/shared/constants/paths'
 import { getAdminLayout } from '@/shared/layouts/admin-layout/admin-layout'
 
 const SuperAdminHome = () => {
-  return <div>SuperAdmin home</div>
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace(RoutersPath.superAdminUsersList)
+  }, [router])
+
+  return <div></div>
 }
 
 SuperAdminHome.getLayout = getAdminLayout
