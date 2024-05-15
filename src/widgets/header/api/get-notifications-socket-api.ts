f50ce1e87e2bae99777f389898ee0c968ab87141
Nikita Alpatov <nikitaa95@mail.ru>
2024-05-15 21:41:44 +0300

    fix: make ws wss

--- src/widgets/header/api/get-notifications-socket-api.ts
+++ src/widgets/header/api/get-notifications-socket-api.ts
@@ -12,7 +12,7 @@ export class GetNotificationsSocketApi {
 
     this.socket = io(
       process.env.NEXT_PUBLIC_NOTIFICATIONS_SOCKET_API || 'https://inctagram.work/',
-      queryParams
+      { transports: ['websocket'], secure: true, query: queryParams }
     )
 
     this.socket.on('connect', () => {
